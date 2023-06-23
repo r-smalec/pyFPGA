ed77635b5642ed00fb34710d0e94a3e64333f112
Radosław Smalec <rsmalec@gmail.com>
2023-06-23 18:07:39 +0200

    Delete pyFPGA_module_init.py

--- pyFPGA_module_init.py
+++
@@ -1,9 +0,0 @@
-import pyperclip
-
-lines_org = pyperclip.paste().split('\n')
-lines_trans = ""
-for line in lines_org:
-    name_var = line.split(' ')[0]
-    lines_trans += name_var + " <= "+ name_var + "\n"
-    print(lines_trans)
-pyperclip.copy(str(lines_trans))
