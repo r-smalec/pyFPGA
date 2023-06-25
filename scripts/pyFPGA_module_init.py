d25ce2d1e0ee46535931ab27b6e1f0d2ad696018
r-smalec <rsmalec@kplabs.pl>
2023-06-25 13:21:02 +0200

    Added beetwen line comment interpretation

--- scripts/pyFPGA_module_init.py
+++ scripts/pyFPGA_module_init.py
@@ -21,44 +21,40 @@ import pyperclip
 lines_original = pyperclip.paste().split('\n')
 lines = ""
 if len(lines_original) > 0:
-    var_dir_prev = lines_original[0].split()[0]
-else:
-    print("\nERROR: no data in cache\n")
-    quit()
-
-if var_dir_prev in ["input", "output", "inout"]:
 
     for line_original_no in range(0, len(lines_original)):
         line = lines_original[line_original_no].split()
         
         if len(line) > 0:
-            comment = ""
-            for i in range(0, len(line)):
-                if line[i].find("//") >= 0: # comment found in a string
-                    comment = ' '.join(line[i:])
-                    line = line[0:i]
-                    break
+            if line[0].find("/") >=0: # line with a comment
+                lines += ' '.join(line) + "\n"
 
-            line[-1] = line[-1].replace(',', '')
-            var_name = line[-1]
-            var_dir = line[0]
-            # uncomment to add empty line between two type of ports
-            # if var_dir != var_dir_prev:
-            #     var_dir = var_dir_prev
-            #     lines += "\n" 
+            else:
+                comment = ""
+                for i in range(0, len(line)):
+                    if line[i].find("//") >= 0: # comment found in a string
+                        comment = ' '.join(line[i:])
+                        line = line[0:i]
+                        break
 
-            if var_dir in ["input", "output", "inout"]:
+                line[-1] = line[-1].replace(',', '')
+
+                var_dir = line[0]
+                var_name = line[-1]
+
+                # uncomment to add empty line between two type of ports
+                # if var_dir != var_dir_prev:
+                #     var_dir = var_dir_prev
+                #     lines += "\n" 
 
                 if line_original_no == len(lines_original) - 1:
                     lines += "\t." + var_name + "("+ var_name + ") " + comment + "\n"
                 else:
                     lines += "\t." + var_name + "("+ var_name + "), " + comment + "\n"
 
-            else:
-                lines += "\n" + ''.join(line) + "\n"
-
     print(lines)
     pyperclip.copy(str(lines))
+
 else:
-    print("\nERROR: invalid data in cache, data should start from: input, output or inout keyword\n")
+    print("\nERROR: no data in cache\n")
     quit()
