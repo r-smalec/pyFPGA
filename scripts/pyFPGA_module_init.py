fce95e3164bd7575df02e5bfeec4493a761cde58
r-smalec <rsmalec@kplabs.pl>
2023-06-25 12:45:08 +0200

    Added in-line comment interpretation

--- scripts/pyFPGA_module_init.py
+++ scripts/pyFPGA_module_init.py
@@ -28,10 +28,17 @@ else:
 
 if last_dir in ["input", "output", "inout"]:
 
-    for line_original in lines_original:
-        line = line_original.split()
+    for line_original_no in range(0, len(lines_original)):
+        line = lines_original[line_original_no].split()
         
         if len(line) > 0:
+            comment = ""
+            for i in range(0, len(line)):
+                if line[i].find("//") >= 0: # comment found in a string
+                    comment = ' '.join(line[i:])
+                    line = line[0:i]
+                    break
+
             line[-1] = line[-1].replace(',', '')
             var_name = line[-1]
             dir = line[0]
@@ -41,11 +48,15 @@ if last_dir in ["input", "output", "inout"]:
             #     lines += "\n" 
 
             if dir in ["input", "output", "inout"]:
-                lines += "  ." + var_name + "("+ var_name + "),\n"
+                
+                if line_original_no == len(lines_original) - 1:
+                    lines += "  ." + var_name + "("+ var_name + ") " + comment + "\n"
+                else:
+                    lines += "  ." + var_name + "("+ var_name + "), " + comment + "\n"
+
             else:
                 lines += "\n" + ''.join(line) + "\n"
 
-    lines = lines[:-2]
     print(lines)
     pyperclip.copy(str(lines))
 else:
