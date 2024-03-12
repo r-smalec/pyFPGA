a222ad17b5cc3677f4f8c7043d902dd758c8002a
rsmalec <rsmalec@kplabs.pl>
2024-03-12 14:30:19 +0100

    constraint generator WIP

--- scripts/pyFPGA_constraints_generate.py
+++ scripts/pyFPGA_constraints_generate.py
@@ -1,4 +1,36 @@
+# Script for generating constraint file for Libero and Vivado using netltst
+# of signals used in the design and Altium Pin Report File generated from 
+# schematic component. 
+# Required is to define appropreiate files one for Pin Report File (pin_mapping_file_ori)
+# second for constraints (.pdc for Libero and .xdc for Vivado)
+
 import csv
+
+def replace_characters_in_pin_mapping(in_file_path, out_file_path):
+    with open(in_file_path, 'r') as in_file:
+        constraints = in_file.readlines()
+        constraints_mod = ""
+        for line in constraints:
+            if any(keyword in line for keyword in ['GND', 'VDD', 'VSS', 'VPP']):
+                pass
+            else:
+                if '\\' in line:
+                    line = line.replace('\\', '')
+                    line = line.split(',')
+                    line[1] = ''.join(line[1:2]) + '_N\n' ## if signal is negated addd _N on the end
+                    line = line[0:2]
+                    constraints_mod = constraints_mod + ','.join(line)
+                else:
+                    line = line.split(',')
+                    if len(str(line[1:2])) > 4: # 4 is for [''] which is empty string
+                        line[1] = ''.join(line[1:2]) # assign to net name
+                        print(type(line[1][-1]))
+                        line[1] = line[1] + '\n'
+                        line = line[0:2] # limit contents to pin designator and net name
+                        constraints_mod = constraints_mod + ','.join(line)
+    
+    with open(out_file_path, 'w') as out_file:
+        out_file.write(constraints_mod)
 
 def search_pin_mapping(pin_mapping_file, search_phrase):
     matching_rows = []
@@ -22,15 +54,19 @@ def search_constraints(in_file_path, out_file_path):
                 if '-pinname' in line or 'PACKAGE_PIN' in line:
                     out_file.write(' '.join(line))
                 else:
-                    print(line[1:2])
+                    pin_name = str(line[1:2])
+                    pin_location = search_pin_mapping(pin_mapping_file_mod, pin_name)
+                    print(pin_name + str(pin_location))
                     #TODO if pdc elif xdc
-                    mod_line = ' '.join(line)[:-1] + " -pinname\n"
+                    mod_line = ' '.join(line)[:-1] + " -pinname \n"
                     out_file.write(mod_line)
             else:
                 out_file.write(' '.join(line))
 
-pin_mapping_file = 'data\pin_mapping.csv' # generated from Altium (Schematic->Component->Right click->Pin Mapping)
+pin_mapping_file_ori = 'data\pin_mapping.csv' # generated from Altium (Schematic->Component->Right click->Pin Mapping)
+pin_mapping_file_mod = 'data\pin_mapping_mod.csv'
 constraints_file_ori = 'data\constraints.pdc' # *.pdc for Libero and *.xdc for Vivado
 constraints_file_mod = 'data\constraints_mod.pdc'
 
-search_constraints(constraints_file_ori, constraints_file_mod)
+replace_characters_in_pin_mapping(pin_mapping_file_ori, pin_mapping_file_mod)
+#search_constraints(constraints_file_ori, constraints_file_mod)
